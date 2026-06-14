73692943493efc52b9328d6cf93697f0712d1662
Galileo <aahmeddgalal@gmail.com>
2026-06-14 06:20:55 +0300

    separate into small components

--- 6-shoppingsite/src/App.jsx
+++ 6-shoppingsite/src/App.jsx
@@ -1,19 +1,20 @@
-import { Routes, Route } from 'react-router'
-import { HomePage } from './pages/HomePage'
-import axios from 'axios';
-import { CheckoutPage } from './pages/CheckoutPage'
-import { OrdersPage } from './pages/OrdersPage'
-import { TrackingPage } from './pages/TrackingPage'
-import { useState, useEffect } from 'react';
-import './App.css'
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/home/HomePage";
+import axios from "axios";
+import { CheckoutPage } from "./pages/Checkout/CheckoutPage";
+import { OrdersPage } from "./pages/orders/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-    const [cart, setCart] = useState([])
-    useEffect(() => {
-      axios.get("/api/cart-items?expand=product").then((response) => { //querey parameter
-        setCart(response.data);
-      });
-    }, [])
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    axios.get("/api/cart-items?expand=product").then((response) => {
+      //querey parameter
+      setCart(response.data);
+    });
+  }, []);
 
   return (
     <Routes>
@@ -26,4 +27,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
---
+++ 6-shoppingsite/src/pages/home/HomePage.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { Header } from "../../components/Header.jsx";
+import { useEffect, useState } from "react";
+import "./HomePage.css";
+import { ProductsGrid } from "./ProductsGrid.jsx";
+
+
+export function HomePage({ cart }) {
+  const [products, setProducts] = useState([]); // Gives us an array of 2 values // is the starting valus of the products
+
+  useEffect(() => {
+    axios
+      .get("/api/products") // Asynchronous code =>> code that doesn't finish right away {request} {a promise}
+      // now responce is not the data so we use the .json to get the data
+      // gives us the data attached to the response and this is also a promise so we have to use (.then)
+      // response.json().then((data) => {}) 
+      .then((response) => {
+        setProducts(response.data);
+      });
+  }, []); //dependancy array if empty array this will let the code run onec and only once and that's what we want
+  // in the console it's run 2wice because of StrictMode and this helps use find bugs
+
+  return (
+    <>
+      <Header cart={cart} />
+
+      <div className="home-page">
+        <ProductsGrid products = {products} />
+      </div>
+    </>
+  );
+}
